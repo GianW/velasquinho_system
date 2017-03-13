97351f5ff6eb8fd9bb726829dce52be92e4fd2e7
Gian Winckler <gian.cb@hotmail.com>
2017-03-13 09:42:43 -0300

    Refatorando

--- app.js
+++ app.js
@@ -1,282 +1,38 @@
+/*jslint node: true */
+/*jslint nomen: true*/
+
+"use strict";
+
 var express = require("express");
 var app = express();
 
-var request = require('request');
+var dados = require('./dados');
 
-const clientes = 'https://mockbin.com/bin/b7cf0b01-cc41-4006-a3b7-965c155d2c3b';
-const compras  = 'https://mockbin.com/bin/bccf6706-3ae0-44ea-8b12-fc45ae236b04';
-
-var lista_clientes = [];
-var lista_compras  = [];
-var lista_produtos = [];
+var cliente_ordem = require('./routes/cliente_ordem.js');
+var cliente_maior = require('./routes/cliente_maior.js');
+var clientes_fieis = require('./routes/clientes_fieis.js');
+var cliente_recomendacao = require('./routes/cliente_recomendacao.js');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/clientes', function(req,res){
-   res.json(lista_clientes);
+app.get('/clientes', function (req, res) {
+    res.json(dados.clientes());
 });
 
-app.get('/compras', function(req,res){
-   res.json(lista_compras);
+app.get('/compras', function (req, res) {
+   res.json(dados.compras());
 });
-
 
 //# 1 - Lista os clientes ordenados pelo valor total de compras
-app.get('/clientes-ordem-compra', function(req,res){
-   somaComprasCliente(function(data){
-      res.json(data);
-      res.end();
-   });
-});
+app.use('/clientes-ordem-compra', cliente_ordem);
 
 //# 2 - Mostre o cliente com maior compra única no último ano (2016).
-app.get('/cliente-maior-compra', function(req,res){
-   //refatorar para pegar por parametro
-   maiorCompraAno('2016', function(data){
-      res.json(data);
-      res.end();
-   });
-});
+app.use('/cliente-maior-compra', cliente_maior);
 
 //# 3 - Liste os clientes mais fiéis.
-app.get('/cliente-fiel', function(req,res){
-   clienteMaisCompras(function(data){
-      console.log('Cliente com mais compras');
-      res.json(data);
-      res.end();
-   });;
-});
-
+app.use('/cliente-fiel', clientes_fieis);
 
 //# 4 - Recomende um vinho para um determinado cliente a partir do histórico
-app.get('/cliente-recomendacao/:cliente', function(req,res){
-   var cliente = req.params.cliente;
-   recomendaCliente(cliente, function(data){
-      console.log('Incidacao de produto');
-      res.json(data);
-      res.end();
-   });
-});
+app.use('/cliente-recomendacao', cliente_recomendacao);
 
 module.exports = app;
-
-function recomendaCliente(cliente, callback){
-   var codCliente = cliente;
-
-   var lista_compras_cliente = [];
-   var produtos_selecionaveis = [];
-   var lista_variedades = [];
-   var lista_categoria = [];
-   var pontos_variedades = [];
-
-   var execLista = new Promise(
-      function(resolve, reject) {
-         lista_compras.filter(
-            function(elem, i, array){
-               if(elem.cliente == codCliente){return true}
-            }).map(function(obj,index){
-               obj.itens.map(function(obj){
-                  lista_compras_cliente.push(obj)
-                  //lista de preferencias
-                  lista_variedades.push(obj.variedade);
-                  lista_categoria.push(obj.categoria);
-               }) ;
-            })
-         resolve('');
-      }
-   );
-   execLista.then(
-      function(){
-         lista_produtos.map(
-            function(obj, index){
-               //monta lista com produtos ainda nao comprados pelo cliente
-               if(verificaObj(lista_compras_cliente, ['produto', obj.produto]) == false){
-                  produtos_selecionaveis.push(obj);
-               }
-               if(index == (lista_produtos.length - 1)){
-                  var variedadeRecomendar = verificaItemQtdMaior(lista_variedades, 'variedade');
-                  variedadeRecomendar.then(function(valor){
-                     //verifica se existe algum produto com essa variedade que o cliente ainda nao comprou
-                     if (verificaObj(produtos_selecionaveis, ['variedade', valor.variedade]) == true) {
-                        produtos_selecionaveis.forEach(function(item,index,array){
-                           if(item.variedade == valor.variedade){
-                              //retorna item indicado
-                              callback(item);
-                           }
-                        })
-                     }else{
-                        //verifica categorias
-                        var categoriaRecomendar = verificaItemQtdMaior(lista_categoria, 'categoria');
-                        categoriaRecomendar.then(function(retorno){
-                           if (verificaObj(produtos_selecionaveis, ['categoria', retorno.categoria]) == true) {
-                              produtos_selecionaveis.forEach(function(item,index,array){
-                                 if(item.categoria == retorno.categoria){
-                                    //retorna item indicado
-                                    callback(item);
-                                 }
-                              })
-                           }
-
-                        });
-                     }
-                  });
-               }
-            }
-         )
-      }
-   );
-}
-
-function verificaItemQtdMaior(arr, tipo){
-   return new Promise(function(resolve, reject){
-     var listaRetorno = [];
-
-    //percorre as variedades que o usuario consome para verificar recorrencias
-    arr.forEach(function(item, index, array){
-      if(verificaObj(listaRetorno, [tipo, item]) == false){
-         if (tipo == 'variedade') {
-            listaRetorno.push({ 'variedade' : item, 'qtd': 1})
-         }else{
-            listaRetorno.push({ 'categoria' : item, 'qtd': 1})
-         }
-      }else{
-         listaRetorno.forEach(function(obj, i, a){
-            if(obj[tipo] == item){
-               obj.qtd += 1;
-            }
-         });
-      }
-      if (index == (arr.length - 1)) {
-
-         var maior = 0;
-         var chave;
-         for(var prop in listaRetorno) {
-            if(listaRetorno[prop].qtd > maior) {
-                maior = listaRetorno[prop].qtd;
-                chave = prop;
-            }
-            if(prop == (listaRetorno.length - 1)){
-               resolve(listaRetorno[chave]);
-            }
-         }
-      }
-   });
-   })
-}
-
-function clienteMaisCompras(callback){
-
-   var valor_clientes = [];
-   var json_clientes  = [];
-
-   lista_compras.map(function(obj,index){
-      if(valor_clientes[obj.cliente]){
-         valor_clientes[obj.cliente] += parseFloat(obj.itens.length);
-      }else{
-         valor_clientes[obj.cliente] = parseFloat(obj.itens.length);
-      }
-      if (index == (lista_compras.length - 1)) {
-          for (var i=0; i< lista_clientes.length; i++){
-            var element = {};
-            element.nome = lista_clientes[i].nome;
-            element.cpf = lista_clientes[i].cpf;
-            element.qtd = valor_clientes[lista_clientes[i].cpf.replace("-", ".")];
-            json_clientes.push(element);
-
-            if (i == (lista_clientes.length - 1)) {
-              json_clientes.sort(function (a, b) {
-                  return b.qtd - a.qtd;
-              });
-              callback(json_clientes[0]);
-            }
-         }
-      }
-   })
-}
-
-function maiorCompraAno(ano, callback){
-   var controle = false;
-
-   var nova_lista = lista_compras.filter(function(elem, i, array){
-      var aux = elem.data.split('-');
-      if (i == (lista_compras.length - 1)){
-         controle = true;
-      }
-      if (aux[2] == ano){return true}
-      else{return false};
-   });
-
-   //refatorar para promise
-   if(controle){
-      nova_lista.sort(function (a, b) {
-         return b.valorTotal - a.valorTotal;
-      });
-      console.log('Maior compra ano: ' + ano);
-      callback(nova_lista[0]);
-   }
-}
-
-
-
-function somaComprasCliente(callback){
-
-   var valor_clientes = [];
-   var json_clientes  = [];
-
-   lista_compras.map(function(obj,index){
-      if(valor_clientes[obj.cliente]){
-         valor_clientes[obj.cliente] += parseFloat(obj.valorTotal);
-      }else{
-         valor_clientes[obj.cliente] = parseFloat(obj.valorTotal);
-      }
-      if (index == (lista_compras.length - 1)) {
-          for (var i=0; i< lista_clientes.length; i++){
-            var element = {};
-            element.nome = lista_clientes[i].nome;
-            element.valor = valor_clientes[lista_clientes[i].cpf.replace("-", ".")];
-            json_clientes.push(element);
-
-            if (i == (lista_clientes.length - 1)) {
-              json_clientes.sort(function (a, b) {
-                  return b.valor - a.valor;
-              });
-              callback(json_clientes);
-            }
-         }
-      }
-   })
-}
-
-function verificaObj(arr, procurar) {
-   var chave = procurar[0];
-   var valor = procurar[1];
-   return !!arr.filter(function (el) {
-     return el[chave] == valor;
-    }).length;
-}
-
-function montarListas(){
-   console.log('Atualizando listas')
-   request(clientes, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-         lista_clientes = JSON.parse(body);
-      }
-   })
-
-   request(compras, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-         lista_compras = JSON.parse(body);
-         lista_compras.map(function(obj,index){
-            obj.itens.map(function(valor){
-               if(verificaObj(lista_produtos, ['produto', valor.produto]) == false){
-                  lista_produtos.push(valor);
-               };
-            })
-         })
-      }
-   })
-
-}
-
-montarListas();
-setInterval(function(){ montarListas() }, 60000);
--- test.js
+++ test.js
@@ -1,11 +1,17 @@
+"use strict"
 var request = require('supertest');
 var app = require('./app');
+var funcoes = require('./funcoes');
 
 describe('Request para as APIS', function(){
   it('API clientes', function(done){
       request(app)
       .get('/clientes')
       .expect('Content-Type', /json/)
+      .end(function(error){
+         if(error) throw error;
+         done();
+      });
    })
   it('Returns JSON format', function(done){
       request(app)
@@ -49,12 +55,36 @@ describe('Teste para as saidas do programa', function(){
    })
    it('Recomendação vinho', function(done){
       request(app)
-      .get('/cliente-recomendacao')
+      .get('/cliente-recomendacao/000.000.000.01')
       .expect(200)
       .end(function(error){
          if(error) throw error;
          done();
       });
    })
-})
+});
+
+describe('Teste das funções do programa', function(){
+
+   it('Verificação se objeto existe no array', function(done){
+
+      var lista = [{'nome': 'teste'},{'nome':'valor'},{'nome':'Valesquinho'},{'nome': ''}];
+      var pesquisa = ['nome', 'Valesquinho'];
+
+      var retorno = funcoes.verificaObj(lista, pesquisa );
+
+      if (retorno) {done()};
+
+   });
+   it('Verificação se objeto não existe no array', function(done){
+
+      var lista = [{'nome': 'teste'},{'nome':'valor'},{'nome':'Valesquinho'},{'nome': ''}];
+      var pesquisa = ['nome', 'System'];
+
+      var retorno = funcoes.verificaObj(lista, pesquisa );
+
+      if (!retorno) {done()};
+
+   });
+});
 
